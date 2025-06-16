db61d427a17797a04d2b4061ba2cf4463c05b578
Juliana Gomonuka <julianagomonjuk@gmail.com>
2025-06-16 10:43:30 +0300

    :Fix: styling the launch button

--- bot-widget.js
+++ bot-widget.js
@@ -472,7 +472,7 @@
     }[detectedLang] || 'Need help?';
 
     const launchButton = document.createElement('button');
-    launchButton.className = 'chat-launcher-button';
+    launchButton.className = `chat-launcher ${settings.style.position === 'left' ? 'left-side' : 'right-side'}`;
     launchButton.innerHTML = `
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
             <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"></path>
@@ -549,7 +549,7 @@
                 action: "sendMessage",
                 sessionId: conversationId,
                 route: settings.webhook.route,
-                chatInput: 'Start conversation',
+                chatInput: detectedLang,
                 metadata: {
                     userId: '',
                     userName: '',
